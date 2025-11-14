166114de8986c1c1509047af5439b7cb14ca2998
GopalMarmat <gopalmarmat256@gmail.com>
2025-11-14 09:47:40 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}", "./app/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'corp-blue': '#0ea5e9',
+        'corp-indigo': '#6366f1'
+      }
+    }
+  },
+  plugins: [],
+}
